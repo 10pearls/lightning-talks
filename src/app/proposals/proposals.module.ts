9963b67f0c062d1f7cf5213268328476bac5e551
dayemsiddiqui <dayem.siddiqui@tenpearls.com>
2019-02-11 11:43:26 +0500

    Basic Structure For Proposal Form

--- src/app/proposals/proposals.module.ts
+++ src/app/proposals/proposals.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApplicationFormComponent } from './application-form/application-form.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ApplicationFormComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule,
+  ],
 })
 export class ProposalsModule { }
